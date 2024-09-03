a42844715ad644f9fb3d67757a0b32f880c6a6ce
Энхжин Баясгалан <24LP1834@G401-CC0100.local>
2024-09-03 12:29:33 +0800

    a little change

--- src/components/home/SinglePostCard.jsx
+++ src/components/home/SinglePostCard.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React from "react";
 import { format } from "date-fns";
 
-export default function SinglePostCard({ title, image, tags, edited_at}) {
+export default function SinglePostCard({ title, image, tags, edited_at }) {
   return (
-    <div className='w-full border rounded-xl'>
-        <img src={image} className='w-full h-80 object-cover rounded-t-xl' />
-        <div className='space-y-4 p-7'>
-            <div className='flex gap-1 text-xs'>
-                {tags.map((tag, idx) => <div key={idx} className='text-[#4B6BFB] rounded-md bg-gray-100 py-1 px-2.5'>{tag}</div>)}
+    <div className="w-full border rounded-xl p-3">
+      <img src={image} className="w-full h-80 object-cover rounded-xl" />
+      <div className="space-y-4 mt-5">
+        <div className="flex gap-1 text-xs">
+          {tags.map((tag, idx) => (
+            <div
+              key={idx}
+              className="text-[#4B6BFB] rounded-md bg-gray-100 py-1 px-2.5"
+            >
+              {tag}
             </div>
-            <h3 className=' text-lg font-bold'>{title}</h3>
-            <span className='text-[#97989F] text-sm'>{format(new Date(edited_at), 'MMMM d, yyy')}</span>
+          ))}
         </div>
+        <h3 className=" text-lg font-bold">{title}</h3>
+        <span className="text-[#97989F] text-sm">
+          {format(new Date(edited_at), "MMMM d, yyy")}
+        </span>
+      </div>
     </div>
-  )
+  );
 }
--- src/components/home/TrendingPosts.jsx
+++ src/components/home/TrendingPosts.jsx
@@ -1,31 +1,39 @@
-import React, { useEffect, useState } from 'react'
-import TrendingPostCard from './TrendingPostCard'
+import React, { useEffect, useState } from "react";
+import TrendingPostCard from "./TrendingPostCard";
 
-const USER_NAME = "gereltuyamz"
-const PER_PAGE = 4
+const USER_NAME = "gereltuyamz";
+const PER_PAGE = 4;
 export default function TrendingPosts() {
-  const [posts, setPosts] = useState([])
+  const [posts, setPosts] = useState([]);
 
   const fetchTrendingPosts = () => {
-    fetch(`https://dev.to/api/articles?username=${USER_NAME}&state=rising&per_page=${PER_PAGE}`)
-      .then(response => response.json())
-      .then(data => {
-        console.log(data)
-        setPosts(data)
-      })
-  }
+    fetch(
+      `https://dev.to/api/articles?username=${USER_NAME}&state=rising&per_page=${PER_PAGE}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setPosts(data);
+      });
+  };
 
   useEffect(() => {
-    fetchTrendingPosts()
-  }, [])
-  
+    fetchTrendingPosts();
+  }, []);
 
   return (
-    <div className='flex flex-col gap-8'>
-      <h2 className=' text-3xl font-bold'>Trending</h2>
-      <div className='grid grid-cols-1 md:grid-cols-4 gap-8'>
-      {posts.map((post, idx) => <TrendingPostCard key={idx} title={post.title} image={post.cover_image} tags={post.tag_list} />)}
+    <div className="flex flex-col gap-8">
+      <h2 className=" text-3xl font-bold">Trending</h2>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        {posts.map((post, idx) => (
+          <TrendingPostCard
+            key={idx}
+            title={post.title}
+            image={post.cover_image}
+            tags={post.tag_list}
+          />
+        ))}
       </div>
     </div>
-  )
+  );
 }
