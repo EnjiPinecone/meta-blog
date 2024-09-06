36aef0039707581b7c64d82429124c2b105aff3e
Энхжин Баясгалан <24LP1834@G401-CC0100.local>
2024-09-06 09:59:42 +0800

    hi

--- src/components/pages/SingleBlogPage.jsx
+++ src/components/pages/SingleBlogPage.jsx
@@ -1,17 +1,15 @@
 import React, { useEffect, useState } from "react";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 
 export default function SingleBlogPage() {
-  const router = useRouter()
-  const postId = router.query.slug
+  const router = useRouter();
+  const postId = router.query.slug;
 
   const [post, setPost] = useState();
   const [page, setPage] = useState(1);
 
   const fetchPost = () => {
-    fetch(
-      `https://dev.to/api/articles/${postId}`
-    )
+    fetch(`https://dev.to/api/articles/${postId}`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
@@ -23,7 +21,15 @@ export default function SingleBlogPage() {
     fetchPost();
   }, [page]);
 
+  if (!post) {
+    return null;
+  }
+
   return (
-    <div>SingleBlogPage: {postId}</div>
-  )
+    <div>
+      <h1 className="text-3xl">{post.title}</h1>
+      <img src={post.user.profile_image} alt="" className="w-9 h-9" />
+      <img src={post.cover_image} alt="" className="w-full h-full rounded-xl" />
+    </div>
+  );
 }
